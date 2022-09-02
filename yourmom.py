1ff727a5c11b2cf653cd1ddb52dcc5bd87704901
Magic2princess <pony200369@gmail.com>
2022-09-02 18:18:41 +0300

    adds x to the for loop

--- yourmom.py
+++ yourmom.py
@@ -5,5 +5,5 @@ print("Your Computer Name is:"+hostname)
 print("Your Computer IP Address is:"+IPAddr)
 from getmac import get_mac_address as gma 
 print(gma())
-for x in range(10):
-    print("the amoung us is sus")
+for x in range(9, 12):
+    print("the amoung us is sus", x)
