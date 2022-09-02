a4a92bb77d6709f996204c7923e90aa4571dee1c
Magic2princess <pony200369@gmail.com>
2022-09-02 18:07:31 +0300

    adds first amongus code

---
+++ yourmom.py
@@ -0,0 +1,9 @@
+import socket   
+hostname=socket.gethostname()   
+IPAddr=socket.gethostbyname(hostname)   
+print("Your Computer Name is:"+hostname)   
+print("Your Computer IP Address is:"+IPAddr)
+from getmac import get_mac_address as gma 
+print(gma())
+for x in range(10):
+    print("the amoung us is sus")
